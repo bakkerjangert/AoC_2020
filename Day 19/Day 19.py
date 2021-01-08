7a1974af29e7adb3ca9229da4c92d2c40ea96a18
Gert Jan Bakker <bakkrjangert@hotmail.com>
2021-01-08 10:11:34 +0100

    Push offline work

--- Day 19/Day 19.py
+++ Day 19/Day 19.py
@@ -2,6 +2,49 @@ import re
 with open('input.txt') as f:
     lines = f.read().splitlines()
 
+
+def get_re(string):
+    regex_list = tuple(string)
+    while True:
+        check = [ele for ele in replacements if(ele in regex_list)]
+        if len(check) == 0:
+            break
+        for val in check[::-1]:
+            val_index = regex_list.index(val)
+            regex_list = regex_list[: val_index] + rules[val] + regex_list[val_index + 1:]
+    return ''.join(regex_list)
+
+
+def get_re_2(string):
+    strings = set()
+    unfinished_strings = [(string,)]
+    while len(unfinished_strings) > 0:
+        cur_string = unfinished_strings.pop(0)
+        while True:
+            for item in cur_string:
+                if item in rules.keys():
+                    index = cur_string.index(item)
+                    rule = rules[item]
+                    if item == '57' or item == '58':
+                        cur_string = cur_string[:index] + (rule[0],) + cur_string[index + 1:]
+                        break
+                    elif '|' in rule and len(rule) == 7:
+                        unfinished_strings.append(cur_string[:index] + (rule[4], rule[5],) + cur_string[index + 1:])
+                        cur_string = cur_string[:index] + (rule[1], rule[2],) + cur_string[index + 1:]
+
+                        break
+                    elif '|' in rules[item]:
+                        unfinished_strings.append(cur_string[:index] + (rule[3],) + cur_string[index + 1:])
+                        cur_string = cur_string[:index] + (rule[1],) + cur_string[index + 1:]
+                        break
+                    else:
+                        cur_string = cur_string[:index] + (rules[item][1],) + (rules[item][2],) + cur_string[index + 1:]
+                        break
+            if cur_string.count('a') + cur_string.count('b') == len(cur_string):
+                strings.add(''.join(cur_string))
+                break
+    return strings
+
 rules = dict()
 messages = list()
 
@@ -20,23 +63,55 @@ for line in lines:
         string = line.split(': ')[1]
         rules[line.split(':')[0]] = tuple('(') + tuple(tuple(string.split(' '))) + tuple(')')
 
-regex_list = tuple('0')
 replacements = tuple(rules.keys())
-
-while True:
-    check = [ele for ele in replacements if(ele in regex_list)]
-    if len(check) == 0:
-        break
-    for val in check[::-1]:
-        val_index = regex_list.index(val)
-        regex_list = regex_list[: val_index] + rules[val] + regex_list[val_index + 1:]
-
-reg_check = re.compile(''.join(regex_list))
-print(''.join(regex_list))
-answer = 0
+reg_check = re.compile(get_re('0'))
+answer = set()
 
 for message in messages:
     if reg_check.fullmatch(message) is not None:
-        answer += 1
+        answer.add(message)
 
-print(f'The answer to part 1 = {answer}')
+print(f'The answer to part 1 = {len(answer)}')
+
+answer = 0
+regex_42 = get_re_2('42')
+regex_31 = get_re_2('31')
+
+for message in messages:
+    # print(f'--- Message = {message} ---')
+    end_42 = False
+    count_42 = 0
+    count_31 = 0
+    for i in range(len(message) // 8):
+        if len(message) < 24:
+            break
+        sub_message = message[0 + i*8: 8 + i*8]
+        # print(sub_message)
+        if i == 0 or i == 1:
+            if sub_message not in regex_42:
+                break
+            # print('Rule 42 found (1st 2 entries)')
+            count_42 += 1
+        elif i == len(message) // 8 - 1:
+            if sub_message not in regex_31:
+                break
+            count_31 += 1
+            if count_42 - 1 < count_31:
+                # print('Rule 31 found (last entrie) --> but too many rules 31 compared to 42')
+                break
+            # print('Rule 31 found (last entrie) --> answer + 1')
+            answer += 1
+        elif (sub_message in regex_42) and (not end_42):
+            # print('Rule 42 found (midsection)')
+            count_42 += 1
+            continue
+        elif sub_message in regex_31:
+            # print('Rule 31 found (midsection)')
+            count_31 += 1
+            end_42 = True
+            continue
+        else:
+            break
+
+print(f'The answer to part 2 = {answer}')
+
--- Day 23/Day 23.py
+++ Day 23/Day 23.py
@@ -1,76 +1,88 @@
-from collections import deque
-data = list(map(int, '364289715'))
-# Testinput
-# data = list(map(int, '389125467'))
-max_val = max(data)
+def print_sequence(cup):
+    org_cup = cup
+    print(cup, end=', ')
+    cup = cups[cup]
+    while cup != org_cup:
+        print(cup, end=', ')
+        cup = cups[cup]
+    print('')
 
-position = 0
+
+data = '364289715'
+# Testdata
+# data = '389125467'
+
+cups = dict()
+
+for i in range(len(data)):
+    val_1 = int(data[i])
+    if i != len(data) - 1:
+        val_2 = int(data[i + 1])
+    else:
+        val_2 = int(data[0])
+    cups[val_1] = val_2
+
+cup = int(data[0])
+max_val = max(cups.keys())
 
 for move in range(100):
-    val = data[position]
-    # shift val to pos[0]
-    data = data[position:] + data[:position]
-    sub_set = data[1:4].copy()
-    del data[1:4]
-    while val not in data[1:]:
-        val -= 1
-        if val == 0:
-            val = max_val
-    index_val = data.index(val) + 1
-    data[index_val:index_val] = sub_set.copy()
-    # shift val back
-    data = data[max_val - position:] + data[:max_val - position]
-    position += 1
-    if position == max_val:
-        position = 0
-    # print(f'--- Move {move + 1} ---')
-    # print(data)
+    # print_sequence(cup)
+    cup_1 = cups[cup]
+    cup_2 = cups[cup_1]
+    cup_3 = cups[cup_2]
+    destination_cup = cup - 1
+    while destination_cup in (cup_1, cup_2, cup_3, 0):
+        if destination_cup == 0:
+            destination_cup = max_val
+        else:
+            destination_cup -= 1
+    cups[cup] = cups[cup_3]
+    cups[cup_3] = cups[destination_cup]
+    cups[destination_cup] = cup_1
+    cup = cups[cup]
 
-index_1 = data.index(1)
-data = data[index_1:] + data[:index_1]
-print(f'The answer to part 1 = {"".join(map(str,data[1:]))}')
+print('The anwer to part 1 = ', end='')
+final_cup = cups[1]
+while final_cup != 1:
+    print(final_cup, end='')
+    final_cup = cups[final_cup]
+print('')
 
 # Part 2
-data = list(map(int, '364289715'))
-# Testinput
-data = list(map(int, '389125467'))
+cups = dict()
+for i in range(len(data)):
+    val_1 = int(data[i])
+    if i != len(data) - 1:
+        val_2 = int(data[i + 1])
+    else:
+        val_2 = int(data[0])
+    cups[val_1] = val_2
 
+val_1 = int(data[-1])
+for i in range(max(cups.keys()) + 1, 1000000 + 1):
+    cups[val_1] = i
+    val_1 = i
+cups[1000000] = int(data[0])
+max_val = 1000000
 
-for i in range(max_val + 1, 1000001):
-    data.append(i)
-max_val = max(data)
-
-data = deque(data)
-
-position = 0
-# print('--- initial position ---')
-# print(f'{data[0:10]} ... {data[index_1: index_1 + 10]} ... {data[position: position + 10]}... {data[-10:]}')
-
+cup = int(data[0])
 for move in range(10000000):
-    # Get subset of 3 items after placeholder
-    sub_set = []
-    for i in range(3):
-        sub_set.append(data[position + 1])
-        del data[position + 1]
-    # Determine new index value
-    val = data[position] - 1
-    while val not in data:
-        val -= 1
-        if val == 0:
-            val = max_val
-    index_val = data.index(val) + 1
-    for sub_val in sub_set:
-        data.insert(index_val, sub_val)
-    position += 1
-    if position == max_val:
-        position = 0
-    if move % 1000 == 0:
-        print(f'Move {move} at {move / 10000000 * 100}%')
-    # print(f'--- Move {move} ---')
-    # print(f'{data[0:10]} ... {data[index_1: index_1 + 10]} ... {data[position: position + 10]}... {data[-10:]}')
+    # print_sequence(cup)
+    cup_1 = cups[cup]
+    cup_2 = cups[cup_1]
+    cup_3 = cups[cup_2]
+    destination_cup = cup - 1
+    while destination_cup in (cup_1, cup_2, cup_3, 0):
+        if destination_cup == 0:
+            destination_cup = max_val
+        else:
+            destination_cup -= 1
+    cups[cup] = cups[cup_3]
+    cups[cup_3] = cups[destination_cup]
+    cups[destination_cup] = cup_1
+    cup = cups[cup]
 
+final_cup_1 = cups[1]
+final_cup_2 = cups[final_cup_1]
 
-index_1 = data.index(1)
-answer = data[(index_1 + 1) % len(data)] * data[(index_1 + 2) % len(data)]
-
-print(f'The answer to part 2 = {answer}')
+print(f'The answer to part 2 = {final_cup_1} x {final_cup_2} = {final_cup_1 * final_cup_2}')
